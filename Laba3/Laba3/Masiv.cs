f622629c0969471b14d24e6da559a2850789d53c
Anastasia Kovalska <s.skaperenn@gmail.com>
2020-03-20 15:41:36 +0200

    .

---
+++ Laba3/Laba3/Masiv.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba3
+{
+    class Masiv
+    {
+        private int[,] masiv = { { 12, 2, 3, 0 }, { 1, 4, 9, 5 }, { 1, 2, 3, 4 }, { 1, 2, 5, 6 } };
+
+        public bool Error = false;
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (Perevirka(i, j))
+                {
+                    return masiv[i, j];
+                }
+
+                else
+                {
+                    Error = true;
+                    return 0;
+                }
+            }
+        }
+        private bool Perevirka(int i, int j)
+        {
+            if ((i >= 0) && (j >= 0) && (i < masiv.GetLength(0)) && (j < masiv.GetLength(1)))
+                return true;
+            else return false;
+        }
+        public int RosmirR
+        {
+            get
+            {
+                return masiv.GetLength(0);
+            }
+        }
+        public int RosmirC
+        {
+            get
+            {
+                return masiv.GetLength(1);
+            }
+        }
+
+    }
+}
---
+++ Laba3/Laba3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Клас інкапсулює двовимірний масив з елементів цілого типу.
+Створити двовимірний індексатор, що повертає відповідне значення
+масиву та запобігає порушенню границі масиву. 
+Створити властивість, доступну для читання закритих елементів-даних, що
+зберігають розміри масиву*/
+namespace Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Masiv masiv = new Masiv();
+
+            var value1 = masiv[0, 3];
+
+            var row = masiv.RosmirR;
+            var col = masiv.RosmirC;
+
+            Console.ReadKey();
+        }
+    }
+}
