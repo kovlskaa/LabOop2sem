8f888ce220616e419884283afaad4ac36d690791
Anastasia Kovalska <s.skaperenn@gmail.com>
2020-04-02 17:08:02 +0300

    3

--- Laba4/Laba4/Vector.cs
+++ Laba4/Laba4/Vector.cs
@@ -7,38 +7,33 @@ using System.Threading.Tasks;
 namespace Laba4
 {
     class Vector
-    {
-        int x, y;
-        public int CoordX
-        {
-            get{return x;}
-        }
-        public int CoordY
-        {
-            get { return y; }
-        }
+    {      
+        public int CoordX { get; set; }
+        public int CoordY { get; set; }
+
         public Vector() { }
         public Vector(int i, int j)
         {
-           x = i;
-           y = j;
+           CoordX = i;
+           CoordY = j;
         }
-        public Vector(Vector vec0) : this(vec0.x, vec0.y) { }
+        public Vector(Vector vec0) : this(vec0.CoordX, vec0.CoordY) { }
+        
         public double Dlina()
         {
-            return Math.Sqrt(Math.Pow(x, 2)+ Math.Pow(y, 2));
+            return Math.Sqrt(Math.Pow(CoordX, 2)+ Math.Pow(CoordY, 2));
         }      
         public static Vector operator *(Vector v, int k)
         {
-            return new Vector(v.x*k,v.y*k);
+            return new Vector(v.CoordX*k,v.CoordY*k);
         }
         public static Vector operator *(int k, Vector v)
         {
-            return new Vector(v.x * k, v.y * k);
+            return new Vector(v.CoordX * k, v.CoordY * k);
         }
         public static Vector operator -(Vector v3, Vector v2)
         {
-            return new Vector(v3.x-v2.x, v3.y - v2.y);
+            return new Vector(v3.CoordX-v2.CoordX, v3.CoordY - v2.CoordY);
         }
     }
 }
